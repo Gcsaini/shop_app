a56e993ddd1aadecd438bc972d87819957b3d20a
Gcsaini <Gopichand0215@gmail.com>
2022-07-17 12:20:48 +0530

    added swipe delete to cart item

--- lib/Providers/cart.dart
+++ lib/Providers/cart.dart
@@ -52,4 +52,9 @@ class Cart with ChangeNotifier {
     });
     return total;
   }
+
+  void removeitem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
 }
--- lib/Widgets/cart_item.dart
+++ lib/Widgets/cart_item.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/Providers/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final String title;
   final double price;
   final int quantity;
 
-  CartItem(this.id, this.title, this.price, this.quantity);
+  CartItem(this.id, this.productId, this.title, this.price, this.quantity);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-          padding: EdgeInsets.all(10),
-          child: ListTile(
-            leading: CircleAvatar(
-              child: Padding(padding: EdgeInsets.all(5) ,child: FittedBox(child: Text('\$${price}'),),) ,
-            ),
-            title: Text(title),
-            subtitle: Text('Total: \$${price*quantity}'),
-            trailing: Text('$quantity'),
-          )),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeitem(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: FittedBox(
+                    child: Text('\$${price}'),
+                  ),
+                ),
+              ),
+              title: Text(title),
+              subtitle: Text('Total: \$${price * quantity}'),
+              trailing: Text('${quantity}X'),
+            )),
+      ),
     );
   }
 }
