4c76f3c9396e314583a1c92834d59ba0b934359b
Gcsaini <Gopichand0215@gmail.com>
2022-07-24 15:24:24 +0530

    animated expanded box

--- lib/Widgets/order_item.dart
+++ lib/Widgets/order_item.dart
@@ -15,38 +15,56 @@ class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text('\$${widget.order.amount}'),
-          subtitle: Text(
-            DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(children: [
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+            ),
           ),
-          trailing: IconButton(
-            onPressed: () {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            }, 
-            icon: Icon(_expanded ? Icons.expand_less: Icons.expand_more),),
-        ),
-        if(_expanded)
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 5,horizontal: 14),
-          height: min(widget.order.products.length*20.0+10, 180),
-          child: ListView(
-            children: widget.order.products.map((prod) =>
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(prod.title,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                Text('\₹${prod.price}',style: TextStyle(fontSize: 18,color: Colors.grey),)
-              ],
-            ),).toList(),
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            height: _expanded
+                ? min(widget.order.products.length * 20.0 + 10, 100)
+                : 0,
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 14),
+            child: ListView(
+              children: widget.order.products
+                  .map(
+                    (prod) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          prod.title,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '\₹${prod.price}',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        )
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
